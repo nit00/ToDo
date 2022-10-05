7906bfb17a7dcd361d0716a7e543965536b04e9c
nit00 <43788725+nit00@users.noreply.github.com>
2022-10-05 21:08:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+    <Home></Home>
+    </>
+  );
+}
+
+export default App;
